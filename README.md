d8e84b2544d3dd83c4333fc58d9a8a4d7c573042
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-11-25 16:25:36 +0100

    add GitHub workflow and simple test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+[![Rust](https://github.com/Zitronenjoghurt/magic-cube/actions/workflows/rust.yml/badge.svg)](https://github.com/Zitronenjoghurt/magic-cube/actions/workflows/rust.yml)
+[![codecov](https://codecov.io/gh/Zitronenjoghurt/magic-cube/graph/badge.svg?token=UM6T22YO17)](https://codecov.io/gh/Zitronenjoghurt/magic-cube)
+![](https://tokei.rs/b1/github/Zitronenjoghurt/magic-cube?category=code&type=Rust&logo=https://simpleicons.org/icons/rust.svg)
+
 # magic-cube
 A rust library providing a magic cube / rubik's cube implementation.
--- src/entities/face.rs
+++ src/entities/face.rs
@@ -2,13 +2,47 @@ use crate::enums::colors::Color;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Face {
-    pub squares: Vec<Color>,
+    size: usize,
+    squares: Vec<Color>,
 }
 
 impl Face {
     pub fn new(size: usize, color: Color) -> Face {
         Face {
+            size,
             squares: vec![color; size],
         }
+    }
+    
+    pub fn size(&self) -> usize {
+        self.size
+    }
+    
+    pub fn squares(&self) -> &[Color] {
+        &self.squares
+    }
+    
+    pub fn squares_mut(&mut self) -> &mut [Color] {
+        &mut self.squares
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_face() {
+        let face = Face::new(9, Color::White);
+        assert_eq!(9, face.size());
+        assert_eq!(Color::White, face.squares()[0]);
+        assert_eq!(Color::White, face.squares()[1]);
+        assert_eq!(Color::White, face.squares()[2]);
+        assert_eq!(Color::White, face.squares()[3]);
+        assert_eq!(Color::White, face.squares()[4]);
+        assert_eq!(Color::White, face.squares()[5]);
+        assert_eq!(Color::White, face.squares()[6]);
+        assert_eq!(Color::White, face.squares()[7]);
+        assert_eq!(Color::White, face.squares()[8]);
     }
 }
